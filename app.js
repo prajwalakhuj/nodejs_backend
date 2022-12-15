366e65c531e44cb70cd29ba773d6d6cf3900fdaa
Prajwal Akhuj <116149622+prajwalakhuj@users.noreply.github.com>
2022-12-15 14:19:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://localhost/conduit');
+  mongoose.connect(process.env.MONGODB_URL_PRAJWAL);
   mongoose.set('debug', true);
 }
 
